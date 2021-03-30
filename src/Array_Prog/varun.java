6bd46d388e3536fe835e235554f0f1c451e4bcda
Nithin1971 <nithingowda6677@gmail.com>
2021-03-30 11:51:47 +0530

    add

--- src/Array_Prog/varun.java
+++ src/Array_Prog/varun.java
@@ -1,11 +1,15 @@
 package Array_Prog;
 
 public class varun {
-public static void main(String[] args) {
-	System.out.println("hi");
-}
-public void add()
-{
-	System.out.println("nithin");
-}
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+	public void add()
+	{
+		System.out.println("nithin");
+	}
+	
+	public void conflict(){
+		System.out.println("thogo");
+	}
 }
