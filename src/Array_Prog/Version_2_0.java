5e7adeebd7141417ea48195243bc4efd8cd51977
Nithin1971 <nithingowda6677@gmail.com>
2021-03-30 11:30:25 +0530

    ver.2

---
+++ src/Array_Prog/Version_2_0.java
@@ -0,0 +1,10 @@
+
+
+package Array_Prog;
+
+public class Version_2_0 {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
---
+++ src/Integer/SumOfInt.java
@@ -0,0 +1,10 @@
+package Integer;
+
+public class SumOfInt {
+
+	public static void main(String[] args) {
+		
+	int amount=1320;
+	System.out.println("1230");
+	}
+}
